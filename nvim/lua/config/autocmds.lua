cdcf021fba78189c56cddbed37edc9dad5301e3f
Bloonstormz <ngminyao@gmail.com>
2026-04-28 13:24:21 +0000

    fix: Load config.local and config.wsl
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,3 +11,12 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
   desc = "Reset Cursor to Beam on leave",
   command = "set guicursor=a:ver20",
 })
+
+-- Load environment-specific extensions when available
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    pcall(require, "config.local")
+    pcall(require, "config.wsl")
+  end,
+})
