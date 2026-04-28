eb2903254d9684fda211aedd849aa99e258a3760
Bloonstormz <ngminyao@gmail.com>
2026-04-28 12:41:05 +0000

    install: Unify link confirmation for files and dirs
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 set -euo pipefail
 
+# If the path provided is a relative path, pre-pend PWD to it else, do nothing
+# This is to gets an absolute path without resolving symlinks unlike `realpath`
+function fake_realpath() {
+    if [[ "$1" =~ ^(/|~) ]]; then
+        echo "$1"
+    else
+        if [[ "$1" =~ ^\.[^.] ]]; then
+            echo "$PWD/${1:2}"
+        else
+            echo "$PWD/$1"
+        fi
+    fi
+}
+
 function link() {
     local src="$(realpath "$1")"
     local dest_dir="$(realpath "$2")"
@@ -19,16 +33,30 @@ function link() {
         shift 2
     fi
 
-    local dest_path="$dest_dir/$dest_name"
+    confirm_link "$src" "$dest_dir/$dest_name"
+}
+
+# 1st argument: Path to source file/directory
+# 2nd argument: Path of the destination link
+#   Note: Name of the destination file/directory should be included in this argument
+function confirm_link() {
+    local src_path="$(fake_realpath "$1")"
+    local dest_path="$(fake_realpath "$2")"
     if [[ -e "$dest_path" || -L "$dest_path" ]]; then
+
+        if [[ -L "$dest_path" ]] && [[ "$(realpath "$dest_path")" == "$(realpath "$src_path")" ]]; then
+            return
+        fi
+
         printf "$dest_path already exists. Override [y/n]?: "
         read -r -n 1 confirm
         printf "\n"
         if [[ $confirm == "y" ]]; then
-            ln -sfT "$src" "$dest_path"
+            rm -rf "$dest_path"
+            ln -sfT "$src_path" "$dest_path"
         fi
     else
-        ln -sT "$src" "$dest_path"
+        ln -sT "$src_path" "$dest_path"
     fi
 }
 
@@ -40,9 +68,7 @@ pushd $SCRIPT_DIR 1>/dev/null
 mapfile -t DIRS < <(find -maxdepth 1 -type d -not -name '.*')
 
 for dir in "${DIRS[@]}"; do
-    if ! [[ -e "$HOME/.config/$dir" ]]; then
-        ln -sn "$(realpath "$dir")" "$HOME/.config/"
-    fi
+    confirm_link "$dir" "$HOME/.config/$dir"
 done
 
 link "$SCRIPT_DIR/bashrc" "$HOME" ".bashrc"
