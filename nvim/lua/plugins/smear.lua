67f293232ca0373155c9386703dca8ba50cbe736
Bloonstormz <ngminyao@gmail.com>
2026-05-06 15:32:11 +0000

    nvim: Add smear-cursor.nvim
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

---
+++ nvim/lua/plugins/smear.lua
@@ -0,0 +1,12 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+
+    min_horizontal_distance_smear = 2,
+    min_vertical_distance_smear = 2,
+  },
+}
