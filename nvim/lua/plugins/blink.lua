d61ff4616b7f9a32fcc32e2d0177799a9cfc4d58
Bloonstormz <ngminyao@gmail.com>
2026-06-04 14:44:35 +0000

    conf(blink): Update blink.cmp keymaps
    Map C-d and C-u to scroll documentation. Unbind blink.cmp's defaults
    
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "fallback" },
+      ["<C-b>"] = { "fallback" },
+    },
+  },
+}
