393e461730263afeca2983dab8e66fa9d1a7d688
Bloonstormz <ngminyao@gmail.com>
2026-06-26 09:05:19 +0000

    conf(ruff): Ignore module imports rule
    Ignore requiring module imports at top of file, as sometimes I want to
    modify the PATH variable before importing
    
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- nvim/after/lsp/ruff.lua
+++ nvim/after/lsp/ruff.lua
@@ -1,8 +1,20 @@
+---@type lspconfig.settings.ruff
+local settings = {
+  lint = {
+    -- Ignore requiring module imports at top of file.
+    -- Sometimes I need to modify the PATH variable before importing
+    ignore = { "E402" },
+  },
+}
+
 ---@type vim.lsp.Config
 return {
   cmd = { require("lib.file").script_dir() .. "/../../../.venv/bin/ruff", "server" },
+  init_options = {
+    settings = settings,
+  },
   ---@type lspconfig.settings.ruff
-  settings = {},
+  settings = settings,
   filetypes = { "python" },
   root_markers = { ".git" },
 }
