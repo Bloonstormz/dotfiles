590ef519c789e71868ed2522c34f27ccbdef836d
Bloonstormz <ngminyao@gmail.com>
2026-06-27 21:14:51 +0100

    chore(wsl): cleanup powershell clipboard paths
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- nvim/lua/config/wsl.lua
+++ nvim/lua/config/wsl.lua
@@ -1,17 +1,22 @@
 -- General Settings for running nvim in wsl
 
+local clip_path = "/mnt/c/Windows/System32/clip.exe"
+local powershell_path = "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
+local powershell_get_clipboard = powershell_path
+  .. ' -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+
 if vim.fn.has("wsl") == 1 then
   -- Copy to clipboard
   vim.g.clipboard = {
     name = "WslClipboard",
     copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
+      ["+"] = clip_path,
+      ["*"] = clip_path,
     },
 
     paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["+"] = powershell_get_clipboard,
+      ["*"] = powershell_get_clipboard,
     },
     cache_enabled = 0,
   }
