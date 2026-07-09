08ac1638b97ffc7681f8715cc88bcdca7eac5cc8
Bloonstormz <ngminyao@gmail.com>
2026-07-09 14:07:51 +0000

    feat(nvim): Add command to copy buffer path
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- nvim/lua/config/cmds.lua
+++ nvim/lua/config/cmds.lua
@@ -1,3 +1,22 @@
 vim.api.nvim_create_user_command("W", "write", {
   desc = "Write",
 })
+
+local function copy_to_clipboard(text)
+  vim.fn.setreg("+", text)
+  vim.notify("Copied '" .. text .. "' to the clipboard")
+end
+
+vim.api.nvim_create_user_command("CopyPath", function()
+  local path = vim.fn.expand("%:p")
+  copy_to_clipboard(path)
+end, {
+  desc = "Copy Absolute Path of Current Buffer",
+})
+
+vim.api.nvim_create_user_command("CopyRelPath", function()
+  local path = vim.fn.expand("%:.")
+  copy_to_clipboard(path)
+end, {
+  desc = "Copy Relative Path of Current Buffer",
+})
