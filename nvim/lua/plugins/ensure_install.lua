8cdd9d8802fc77b07e043307eb415dca8c5f564c
Bloonstormz <ngminyao@gmail.com>
2026-07-23 13:55:57 +0000

    WIP: Only mason install if requirement is present
    Signed-off-by: Bloonstormz <ngminyao@gmail.com>

--- nvim/lua/plugins/ensure_install.lua
+++ nvim/lua/plugins/ensure_install.lua
@@ -1,3 +1,13 @@
+local mason_require = {
+  vtsls = "npm",
+  biome = "npm",
+  pyright = "npm",
+  ["tailwindcss-language-server"] = "npm",
+  ["json-lsp"] = "npm",
+  ["bash-language-server"] = "npm",
+  clangd = "unzip",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -11,10 +21,19 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "ast-grep",
-      },
-    },
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "ast-grep")
+
+      local new_installed = {}
+
+      for _, v in ipairs(opts.ensure_installed) do
+        local requirement = mason_require[v]
+        if requirement == nil or vim.fn.executable(requirement) then
+          table.insert(new_installed, v)
+        end
+      end
+    end,
   },
 }
